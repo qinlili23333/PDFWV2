51383127d557106e8e20825b85e3c5a676d9ecd1
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-04-21 22:45:26 +1000

    [cfg] Add detailed summary and Fallback option

--- PDFWV2/PDFWV2Options.cs
+++ PDFWV2/PDFWV2Options.cs
@@ -8,17 +8,31 @@
     }
     public enum Engines
     {
+        EDGE,
         PDFJS
     }
     public class PDFWV2Options
     {
-        //Module folder
+        /// <summary>
+        /// Module folder, the location where PDFWV2 stores data, shared across applications by default, but you can set to another folder if you really need fully isolation.
+        /// </summary>
         public string ModuleFolder { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\PDFWV2";
-        //Target PDF Engine, currently 'pdfjs' is the only acceptable, more engines in future updates
+        /// <summary>
+        /// Target PDF Engine, currently 'pdfjs' (default) and 'edge' are the only acceptable, more engines in future updates
+        /// </summary>
         public Engines Engine { get; set; } = Engines.PDFJS;
-        //Enable F12 DebugTool
+        /// <summary>
+        /// Enable F12 DebugTool, disabled by default
+        /// </summary>
         public bool DebugTool { get; set; } = false;
-        //Update mode
+        /// <summary>
+        /// Update mode, background update by default
+        /// </summary>
         public UpdateMode EnableUpdate { get; set; } = UpdateMode.Background;
+        /// <summary>
+        /// Whether to fallback to Edge PDF engine when another render cannot be initialized, enabled by default.
+        /// Has no impact if Edge is already the selected engine
+        /// </summary>
+        public bool FallbackToEdge { get; set; } = true;
     }
 }

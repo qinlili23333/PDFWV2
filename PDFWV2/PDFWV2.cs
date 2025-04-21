6d6fe89cd9278e304620c2056514960fba13dd7a
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-04-21 23:34:40 +1000

    [wv2] Basic WebView2 lifecycle management

--- PDFWV2/PDFWV2.cs
+++ PDFWV2/PDFWV2.cs
@@ -1,12 +1,70 @@
-﻿namespace PDFWV2
+﻿using Microsoft.Web.WebView2.Core;
+using System.IO;
+
+namespace PDFWV2
 {
     public class PDFWV2
     {
-        public PDFWV2() : this(new PDFWV2Options())
+        internal PDFWV2(PDFWV2Options Options)
         {
+            Directory.CreateDirectory(Options.ModuleFolder);
         }
-        public PDFWV2(PDFWV2Options Options)
+
+        public async Task<PDFWV2> CreateInstance()
         {
+            return await CreateInstance(new PDFWV2Options());
+        }
+
+        public async Task<PDFWV2> CreateInstance(PDFWV2Options Options)
+        {
+            if (PDFWV2InstanceManager.Instance != null)
+            {
+                throw new Exception("Double Initialization! You can only initialize one instance in one process.");
+            }
+            PDFWV2InstanceManager.Instance = new PDFWV2(Options);
+            await InitAppWebView(Options.ModuleFolder);
+            return PDFWV2InstanceManager.Instance;
+        }
+
+        public async Task<bool> Dispose(bool Force)
+        {
+            if (PDFWV2InstanceManager.Instance == null)
+            {
+                throw new NullReferenceException("Null Instance! You can only dispose an initialized instance.");
+            }
+            if (PDFWV2InstanceManager.ActiveDocuments.Count > 0 && !Force)
+            {
+                //Still has active documents
+                return false;
+            }
+            var tcs = new TaskCompletionSource<bool>();
+            PDFWV2InstanceManager.WebView2Environment.BrowserProcessExited += (o, e) =>
+            {
+                tcs.TrySetResult(true);
+            };
+            PDFWV2InstanceManager.AliveController.Close();
+            await tcs.Task;
+            return true;
+        }
+        private async static Task InitAppWebView(string Path)
+        {
+            if (PDFWV2InstanceManager.WebView2Environment != null)
+            {
+                return;
+            }
+            else
+            {
+                var WebviewArgu = "--disable-features=msSmartScreenProtection --enable-features=msWebView2EnableDraggableRegions --in-process-gpu --disable-web-security --no-sandbox";
+                CoreWebView2EnvironmentOptions options = new()
+                {
+                    AdditionalBrowserArguments = WebviewArgu
+                };
+                Directory.CreateDirectory(Path + @"\WebviewCache\");
+                PDFWV2InstanceManager.WebView2Environment = await CoreWebView2Environment.CreateAsync(null, Path + @"\WebviewCache\", options);
+                //Create hidden WebView2 on MSG HWND to keep alive
+                IntPtr HWND_MESSAGE = new(-3);
+                PDFWV2InstanceManager.AliveController = await PDFWV2InstanceManager.WebView2Environment.CreateCoreWebView2ControllerAsync(HWND_MESSAGE);
+            }
         }
     }
 }
---
+++ PDFWV2/PDFWV2InstanceManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Web.WebView2.Core;
+
+namespace PDFWV2
+{
+    /// <summary>
+    /// Internal instance manager stores all static variables need in same process.
+    /// One process can only initialize one PDFWV2 instance.
+    /// </summary>
+    internal static class PDFWV2InstanceManager
+    {
+        /// <summary>
+        /// PDFWV2 instance in process.
+        /// </summary>
+        internal static PDFWV2? Instance;
+        /// <summary>
+        /// WebView2 environment for whole process.
+        /// </summary>
+        internal static CoreWebView2Environment? WebView2Environment;
+        /// <summary>
+        /// The hidden WebView2 controller to keep WebView2 environment always ready.
+        /// Fucking Microsoft doesn't provide keep alive feature and just destroys environment after all pages closed.
+        /// </summary>
+        internal static CoreWebView2Controller? AliveController;
+        /// <summary>
+        /// A list of active documents.
+        /// Instance can only be destroyed with no active document.
+        /// </summary>
+        internal static List<PDFWindow> ActiveDocuments = [];
+    }
+}

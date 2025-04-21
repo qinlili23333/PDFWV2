51cac7e45ba2997dd40a18c64ea974eea71e1cb8
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-04-21 17:42:56 +1000

    [cfg] Add Options

---
+++ PDFWV2/PDFWV2Options.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFWV2
+{
+    public enum UpdateMode
+    {
+        Never,
+        Background,
+        Foreground
+    }
+    public class PDFWV2Options
+    {
+        //Module folder
+        public string ModuleFolder { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\PDFWV2";
+        //Target PDF Engine, currently 'pdfjs' is the only acceptable, more engines in future updates
+        public string Engine { get; set; } = "pdfjs";
+        //Enable F12 DebugTool
+        public bool DebugTool { get; set; } = false;
+        //Update mode
+        public UpdateMode EnableUpdate { get; set; } = UpdateMode.Background;
+    }
+}

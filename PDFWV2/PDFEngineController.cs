e95995908000b80ff77102703851f33572529a28
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-04-22 15:19:36 +1000

    [eng] Basic structure for engine controller

---
+++ PDFWV2/PDFEngineController.cs
@@ -0,0 +1,15 @@
+﻿namespace PDFWV2
+{
+    internal abstract class PDFEngineController
+    {
+        internal PDFEngineController()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Call when PDFWindow successfully created WebView2Controller
+        /// </summary>
+        internal abstract void OnWebViewReady();
+    }
+}
---
+++ PDFWV2/PDFEngines/EdgeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFWV2.PDFEngines
+{
+    internal class EdgeController : PDFEngineController
+    {
+        internal EdgeController(Stream Stream) 
+        { 
+
+        }
+        internal EdgeController(string Path)
+        {
+
+        }
+
+        internal override void OnWebViewReady()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

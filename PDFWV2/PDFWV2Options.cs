af1f317b8455e6ddd12e3025241870aba3ccf8f7
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-04-22 13:10:47 +1000

    [cfg] Add NetworkRequestIsolation

--- PDFWV2/PDFWV2Options.cs
+++ PDFWV2/PDFWV2Options.cs
@@ -34,5 +34,12 @@
         /// Has no impact if Edge is already the selected engine
         /// </summary>
         public bool FallbackToEdge { get; set; } = true;
+        /// <summary>
+        /// Whether to enable network request isolation.
+        /// If enabled, all network requests will be done in main process and convert to memory stream for PDF engine. All network request from PDF engine will be blocked.
+        /// This enhances security to prevent remote execution from WebView2, but may impact performance.
+        /// Enabled by default and not recommend to disable.
+        /// </summary>
+        public bool NetworkRequestIsolation { get; set; } = true;
     }
 }

6144b4b128be84cb187c8bb33afbeed6c3d456aa
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-07-19 09:41:14 +1000

    [eng|adobe] Dummy class for adobe engine

---
+++ PDFWV2/PDFEngines/Adobe.cs
@@ -0,0 +1,61 @@
+﻿using PDFWV2.Utils;
+using System.IO;
+
+namespace PDFWV2.PDFEngines
+{
+    internal class Adobe : PDFEngine
+    {
+        public Adobe() : base(string.Empty)
+        {
+
+        }
+        /// <inheritdoc />
+        public override UpdateResult Update()
+        {
+            // Adobe PDF engine should always up to date.
+            return UpdateResult.NoUpdate;
+        }
+
+        /// <inheritdoc />
+        protected override PDFWindow ViewFileEngine(string Path)
+        {
+            // There is no need to convert to stream for Adobe PDF
+            // Just directly open it
+            return new PDFWindow(new AdobeController(new System.Uri(Path).AbsoluteUri));
+        }
+
+        /// <inheritdoc />
+        public override PDFWindow ViewStream(Stream Stream)
+        {
+            return new PDFWindow(new AdobeController(Stream));
+        }
+
+        /// <inheritdoc />
+        public override PDFWindow ViewURL(string URL)
+        {
+            if (PDFWV2InstanceManager.Options.NetworkRequestIsolation)
+            {
+                var Controller = new AdobeController();
+                GetFileAndFulfill(Controller, URL);
+                return new PDFWindow(Controller);
+            }
+            else
+            {
+                return new PDFWindow(new AdobeController(URL));
+            }
+        }
+
+        private async void GetFileAndFulfill(AdobeController Controller, string URL)
+        {
+            Stream ContentStream = await Network.GetHttpStream(URL);
+            Controller.FulfillStream(ContentStream);
+        }
+
+        /// <inheritdoc />
+        public override bool IsReady()
+        {
+            return true;
+        }
+    }
+
+}
---
+++ PDFWV2/PDFEngines/AdobeController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System.IO;
+
+namespace PDFWV2.PDFEngines
+{
+    internal class AdobeController : PDFEngineController
+    {
+        private Stream? DocumentStream;
+        private string DocumentPath = string.Empty;
+        private bool PreloadMode = false;
+        private PDFWindow? PDFWindow;
+
+        private bool Initialized = false;
+        private TaskCompletionSource<bool> InitializeTCS = new();
+
+        /// <summary>
+        /// Create with ready stream
+        /// </summary>
+        /// <param name="Stream">Stream, prefer MemoryStream</param>
+        internal AdobeController(Stream Stream)
+        {
+            DocumentStream = Stream;
+            LoadStream(Stream);
+        }
+
+        /// <summary>
+        /// Create with URL path, can be local file URI or online link URL
+        /// </summary>
+        /// <param name="Path"></param>
+        internal AdobeController(string Path)
+        {
+            DocumentPath = Path;
+        }
+
+        /// <summary>
+        /// Create empty controller and fulfill content later
+        /// </summary>
+        internal AdobeController()
+        {
+            PreloadMode = true;
+        }
+
+        /// <summary>
+        /// Fulfill stream after controller loaded
+        /// </summary>
+        /// <param name="Stream">File stream</param>
+        internal void FulfillStream(Stream Stream)
+        {
+            if (PreloadMode)
+            {
+                DocumentStream = Stream;
+                PreloadMode = false;
+                LoadStream(Stream);
+            }
+            else
+            {
+                throw new InvalidOperationException("You cannot fulfill stream to a loaded controller!");
+            }
+        }
+
+        private async Task WaitReady()
+        {
+        }
+
+        /// <summary>
+        /// Load stream to engine by rewriting response
+        /// </summary>
+        /// <param name="Stream">File stream</param>
+        private async Task LoadStream(Stream Stream)
+        {
+            await WaitReady();
+            PDFWindow.WebView.CoreWebView2.AddWebResourceRequestedFilter(
+      "*", CoreWebView2WebResourceContext.Document);
+            PDFWindow.WebView.CoreWebView2.WebResourceRequested += delegate (
+               object? sender, CoreWebView2WebResourceRequestedEventArgs args)
+            {
+                // Edge PDF has nothing to save on same PDF, so no need for hashed URL to enable progress saving
+                if (args.Request.Uri == $"https://{PDFWV2InstanceManager.LocalDomain}/Stream.pdf")
+                {
+                    CoreWebView2WebResourceResponse response = PDFWindow.WebView.CoreWebView2.Environment.CreateWebResourceResponse(Stream, 200, "OK", "Content-Type: application/pdf");
+                    args.Response = response;
+                }
+            };
+            PDFWindow.WebView.CoreWebView2.Navigate($"https://{PDFWV2InstanceManager.LocalDomain}/Stream.pdf");
+        }
+
+        internal override void OnWebViewReady(PDFWindow Window)
+        {
+            PDFWindow = Window;
+        }
+
+        internal override void Dispose()
+        {
+            DocumentStream?.Dispose();
+        }
+    }
+}
